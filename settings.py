90611239abf2f6825914fe2bdcdd8184889fc1fd
Cynamonek <imac@imac-imac.home>
2024-12-16 10:46:46 +0100

    New map

--- settings.py
+++ settings.py
@@ -1,14 +1,18 @@
 world_map = [
-	'                                                                  ',
-	'                                                                  ',
-	'                                                                  ',
-	' X   X  X    xXXXXXXXXXs                   XX   X                 ',
-	' X XXX      X         XX                XXXX    XX                ',
-	'  L       c                                 XXXXX                 ',
-	'          X       c            t   X                           G  ',
-	'     Xc XXXXXX  XXXXs    XXXXXXXXXXX  XX                      XXXX',
-	'XP       X XX X  X XXX      X XX  XX  XXX  XXXXXXXXs  XXXXXX      ',
-	'FFFFFFFFFF  F F  F  FFFFFFFFF FF  FF  FFF  FF FF FFFFFFF  F       ',
+'                                                                                                                                                                                                                       ',
+'                                                                                                                                                                                                                       ',
+'                                                                                                                                                                                                                       ',
+'                                                                                                                                                                                                                       ',
+'                                                                                                                                                                                                             X         ',
+'                                                                 k  ccc                      o           XXX   XXLLX                                                                                        XX         ',
+'                      L                                        XXXXXXXXXX                   L                                                                                                              XXX         ',
+'                                                                                                                                                                                                          XXXX         ',
+'                                                                                                               XX                                     X                                                  XXXXX         ',
+'                    g                                     XLX                 X   XX     L  L  L    X                         X     X                XX    X                    XXXLXX                  XXXXXX         ',
+'         L        XXLXXLX                  X                                                                                 XX c X XX              XXX    XX                                          XXXXXXX         ',
+'                                      X  o X                                                                                XXX X c XXX            XXXX    XXX            XX                          XXXXXXXX         ',
+' P                    ccc       X     X    X                        ccc                    cc           cc                 XXXX c o XXXX     k    XXXXX    XXXX           XX          ooo       kk   XXXXXXXXX      G  ',
+'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF  FFFFFFFFFFFFFFFFFFFF  FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF    FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
 ]
 
 tile_size = 50
